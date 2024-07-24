896acd851bc3a94f1a13ba3cd113be2d921af8c1
Florian Rumpel <florianrumpel@yahoo.com>
2024-07-24 21:46:11 +0200

    add structured data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,21 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.scss";
 import Layout from "@/components/Navigation";
-
 const inter = Inter({subsets: ["latin"]});
+import {BlogPosting, WithContext} from "schema-dts";
+import Script from "next/script";
+
+const jsonLd: WithContext<BlogPosting> = {
+  "@context": "https://schema.org",
+  "@type": "BlogPosting",
+  name: "Emelie Christina Trenkler",
+  url: "https://emelie-christina-trenkler.vercel.app/",
+  description: "Der Blog von Emelie Christina Trenkler",
+  publisher: {
+    "@type": "Person",
+    name: "Emelie Christina Trenkler",
+  },
+};
 
 export const metadata: Metadata = {
   title: {
@@ -21,6 +34,13 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body className={inter.className}>
+        <Script
+          id="faq-schema"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(jsonLd),
+          }}
+        />
         <Layout />
         {children}
       </body>
